57e089b1d50e68f0022d2121a92f5c911ced673d
jbaumotte <jbaumotte@gmail.com>
2013-10-15 14:16:36 +0000

    first commit

---
+++ controllers/default.py
@@ -0,0 +1,77 @@
+# -*- coding: latin1 -*-
+
+def index():
+   
+    form = SQLFORM.factory(Field('processo', label=T('Numero do Processo: ')))
+    
+    if form.process().accepted:
+            
+        import tj
+        processo = tj.get_page(form.vars.processo)
+        session.reu = processo[0].decode("latin1")
+        session.classe = processo[1].decode("latin1")
+        session.numero = processo[2]
+        session.crime = processo[3].decode("latin1")
+        session.competencia = processo[4]
+        redirect(URL('cadastro'))
+
+    return dict(form=form)
+    
+@auth.requires_login()
+def cadastro():
+    db.processo.insert(reu=session.reu, classe=session.classe, crime = session.crime, competencia=session.competencia, numero=session.numero)
+    return dict()
+
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/manage_users (requires membership in 
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
+
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
+
+
+@auth.requires_signature()
+def data():
+    """
+    http://..../[app]/default/data/tables
+    http://..../[app]/default/data/create/[table]
+    http://..../[app]/default/data/read/[table]/[id]
+    http://..../[app]/default/data/update/[table]/[id]
+    http://..../[app]/default/data/delete/[table]/[id]
+    http://..../[app]/default/data/select/[table]
+    http://..../[app]/default/data/search/[table]
+    but URLs must be signed, i.e. linked with
+      A('table',_href=URL('data/tables',user_signature=True))
+    or with the signed load operator
+      LOAD('default','data.load',args='tables',ajax=True,user_signature=True)
+    """
+    return dict(form=crud())
---
+++ models/db.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# request.requires_https()
+
+if not request.env.web2py_runtime_gae:
+    ## if NOT running on Google App Engine use SQLite or other DB
+    #from gluon.contrib.heroku import *
+    #try: db = DAL(os.environ.get('DATABASE_URL'))
+    #except: db = get_db(name=None, pool_size=10)
+    db = DAL('sqlite://storage.db')
+else:
+    db = DAL('google:datastore')
+    session.connect(request, response, db=db)
+
+
+from gluon.tools import Auth
+auth = Auth(db)
+auth.define_tables(username=True)
+
+db.define_table('processo',
+   Field('criado_em', 'datetime', default=request.now),
+   Field('classe'),
+   Field('numero'),
+   Field('competencia'),
+   Field('reu'),
+   Field('crime'))
